f4eed7c7fb265c8936b2d8131126ef7f266775e7
TommasoBrivio <tommasobrivio150@gmail.com>
2025-05-08 14:30:17 +0200

    Elenco dei casi d'uso

--- src/main/java/it/itsincom/webdevd/web/UserResource.java
+++ src/main/java/it/itsincom/webdevd/web/UserResource.java
@@ -3,6 +3,8 @@ package it.itsincom.webdevd.web;
 import it.itsincom.webdevd.service.UserService;
 import it.itsincom.webdevd.web.model.CreateUserRequest;
 import it.itsincom.webdevd.web.model.UserResponse;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
@@ -20,14 +22,21 @@ public class UserResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
     public UserResponse register(CreateUserRequest request) {
         return userService.createUser(request);
     }
 
-
+    //diventa stampa con id facoltativo
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<UserResponse> findAll() {
+    @PermitAll
+    @Path("/{id}")
+    public List<UserResponse> findAll(@PathParam("id") String id) {
         return userService.findAll();
     }
+
+    //modifica(PUT "/{id}") con id obbligatorio, se ruolo user e id è il suo, se admin non si controlla id
+
+    //elimina solo se admin, id obbligatorio
 }
